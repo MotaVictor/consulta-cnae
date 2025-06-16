d165d2948d4c31b30ce5880075713f0fb26460b5
MotaVictor <158361383+MotaVictor@users.noreply.github.com>
2025-06-16 20:13:56 -0300

    Update consulta_cnae.py

--- consulta_cnae.py
+++ consulta_cnae.py
@@ -25,10 +25,11 @@ if codigo:
     resultados = df[df["CNAE"] == codigo]
 
     if not resultados.empty:
-        st.success(f"Classes encontradas para o CNAE {codigo}:")
-        descricoes = resultados["CNT_Classe"].unique()
-        for descricao in descricoes:
-            st.markdown(f"• {descricao}")
+    st.success(f"Classes encontradas para o CNAE {codigo}:")
+    descricoes = resultados["CNT_Classe"].unique()
+    for descricao in descricoes:
+        st.markdown(f"<p style='margin-bottom: 25px;'>• {descricao}</p>", unsafe_allow_html=True)
+
     else:
         st.warning("Nenhuma classe encontrada para este código CNAE.")
         st.markdown("<p style='color:gray;'>Verifique se digitou corretamente os 7 números. Exemplo válido: <code>1041400</code>.</p>", unsafe_allow_html=True)
